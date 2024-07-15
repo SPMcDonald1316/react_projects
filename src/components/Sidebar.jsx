1ff65db44295c6b749d184f565e2fbc9fd3ded01
sean <spmcdonald1316@gmail.com>
2024-07-15 13:19:31 -0500

    create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+
+function Sidebar() {
+  return (
+    <Navbar sticky="top" className="flex-column Sidebar">
+      <Nav.Item>
+        <Nav.Link href="/">Feed</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/explore">Explore</Nav.Link>
+      </Nav.Item>
+    </Navbar>
+  );
+}
+
+export default Sidebar;
