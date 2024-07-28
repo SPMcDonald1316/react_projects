639db096dd16b8980d9407c4c506238ea8370dee
sean <spmcdonald1316@gmail.com>
2024-07-27 21:06:25 -0500

    complete time ago component

--- src/components/TimeAgo.jsx
+++ src/components/TimeAgo.jsx
@@ -31,6 +31,22 @@ function getTimeAgo(date) {
   return [bestTime, bestUnit, bestInterval]
 }
 
-export default function TimeAgo({ isoDate }) {
-  // TODO
+function TimeAgo({ isoDate }) {
+  const date = new Date(Date.parse(isoDate));
+  const [time, unit, interval] = getTimeAgo(date);
+  const [, setUpdate] = useState(0);
+
+  useEffect(() => {
+    const timerId = setInterval(
+      () => setUpdate(update => update + 1),
+      interval * 1000
+    );
+    return () => clearInterval(timerId);
+  }, [interval]);
+
+  return (
+    <span title={date.toString()}>{rtf.format(time, unit)}</span>
+  );
 }
+
+export default TimeAgo
