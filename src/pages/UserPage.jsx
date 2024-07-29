c3e42e8cbcbacfe3779c368e142fc671d0528cda
sean <spmcdonald1316@gmail.com>
2024-07-29 11:02:16 -0500

    build out user page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,13 +1,46 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useApi } from '../contexts/ApiProvider';
+import { Stack, Image, Spinner } from "react-bootstrap";
 import Body from '../components/Body';
+import TimeAgo from '../components/TimeAgo';
 
 function UserPage() {
   const { username } = useParams();
+  const [user, setUser] = useState();
+  const api = useApi();
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get(`/users/${username}`);
+      setUser(response.ok ? response.body : null);
+    })();
+  }, [username, api]);
 
   return (
     <Body sidebar>
-      <h1>{username}</h1>
-      <p>TODO</p>
+      {user === undefined ?
+        <Spinner animation="border" />
+      :
+        <>
+          {user === null ? 
+            <p>User not found.</p>
+          :
+            <Stack direction="horizontal" gap={4}>
+              <Image src={user.avatar_url + '&s=128'} roundedCircle />
+              <div>
+                <h1>{user.username}</h1>
+                {user.about_me && <h5>{user.about_me}</h5>}
+                <p>
+                  Member since: <TimeAgo isoDate={user.first_seen} />
+                  <br />
+                  Last seen: <TimeAgo isoDate={user.last_seen} />
+                </p>
+              </div>
+            </Stack>
+          }
+        </>
+      }
     </Body>
   );
 }
