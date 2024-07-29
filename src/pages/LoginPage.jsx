b93b4afaf848ba314259a4b73fdd613cb1cc4f6e
sean <spmcdonald1316@gmail.com>
2024-07-29 14:53:25 -0500

    use input field component to build out login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { Form, Button } from 'react-bootstrap';
 import Body from "../components/Body";
+import InputField from '../components/InputField';
 
 function LoginPage() {
+  const [formErrors, setFormErrors] = useState();
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    console.log('handle form here');
+  };
+
   return (
     <Body>
-      <h1>Login form</h1>
-      <p>TODO</p>
+      <h1>Login</h1>
+      <Form onSubmit={onSubmit}>
+        <InputField
+          name="username"
+          label="Username or email address"
+          error={formErrors && formErrors.username}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          error={formErrors && formErrors.password}
+        />
+        <Button variant="primary" type="submit">Login</Button>
+      </Form>
     </Body>
   );
 }
