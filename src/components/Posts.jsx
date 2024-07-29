d88d4dc3e204798f83e5f2444b932f0be636e424
sean <spmcdonald1316@gmail.com>
2024-07-29 11:29:24 -0500

    make posts component more flexible by using prop to display different lists of posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,20 +3,34 @@ import { Spinner } from "react-bootstrap";
 import { useApi } from '../contexts/ApiProvider';
 import Post from "./Post";
 
-function Posts() {
+function Posts({ content }) {
   const [posts, setPosts] = useState();
   const api = useApi();
 
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed'
+      break;
+    case 'explore':
+      url = '/posts';
+      break;
+    default:
+      url =  `/users/${content}/posts`;
+      break;
+  }
+
   useEffect(() => {
     (async () => {
-      const response = await api.get('/feed');
+      const response = await api.get(url);
       if (response.ok) {
         setPosts(response.body.data)
       } else {
         setPosts(null);
       }
     })();
-  }, [api]);
+  }, [api, url]);
 
   return (
     <>
