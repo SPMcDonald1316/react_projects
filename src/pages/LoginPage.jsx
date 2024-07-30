f276dd25ddca7ad7cf5582f7f26bde0d2c3bacac
sean <spmcdonald1316@gmail.com>
2024-07-30 13:29:39 -0500

    setup references to access data from form fields

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Form, Button } from 'react-bootstrap';
 import Body from "../components/Body";
 import InputField from '../components/InputField';
 
 function LoginPage() {
   const [formErrors, setFormErrors] = useState({});
+  const usernameField = useRef();
+  const passwordField = useRef();
+
+  useEffect(() => {
+    usernameField.current.focus();
+  }, []);
 
   const onSubmit = (ev) => {
     ev.preventDefault();
-    console.log('handle form here');
+    const username = usernameField.current.value;
+    const password = passwordField.current.value;
+
+    console.log(`You entered ${username}:${password}`);
   };
 
   return (
@@ -19,12 +28,14 @@ function LoginPage() {
           name="username"
           label="Username or email address"
           error={formErrors.username}
+          fieldRef={usernameField}
         />
         <InputField
           name="password"
           label="Password"
           type="password"
           error={formErrors.password}
+          fieldRef={passwordField}
         />
         <Button variant="primary" type="submit">Login</Button>
       </Form>
