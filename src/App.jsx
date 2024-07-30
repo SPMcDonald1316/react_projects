12dd9395819b5951ed225ce6641c2008f540aa84
sean <spmcdonald1316@gmail.com>
2024-07-30 15:53:41 -0500

    add flash context to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ApiProvider from './contexts/ApiProvider';
+import FlashProvider from './contexts/FlashProvider';
 import Header from './components/Header';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
@@ -12,17 +13,19 @@ function App() {
   return (
     <Container fluid className='App'>
       <BrowserRouter>
-        <ApiProvider>
-          <Header />
-          <Routes>
-            <Route path='/' element={<FeedPage />} />
-            <Route path='/explore' element={<ExplorePage />} />
-            <Route path='/user/:username' element={<UserPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegistrationPage />} />
-            <Route path='*' element={<Navigate to="/" />} />
-          </Routes>
-        </ApiProvider>
+        <FlashProvider>
+          <ApiProvider>
+            <Header />
+            <Routes>
+              <Route path='/' element={<FeedPage />} />
+              <Route path='/explore' element={<ExplorePage />} />
+              <Route path='/user/:username' element={<UserPage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/register' element={<RegistrationPage />} />
+              <Route path='*' element={<Navigate to="/" />} />
+            </Routes>
+          </ApiProvider>
+        </FlashProvider>  
       </BrowserRouter>
     </Container>
   );
