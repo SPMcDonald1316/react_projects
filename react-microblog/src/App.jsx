5c9570cb620c3239f3c6f9cfb3301d6761c17716
sean <spmcdonald1316@gmail.com>
2024-07-31 15:42:29 -0500

    add user context to app component

--- react-microblog/src/App.jsx
+++ react-microblog/src/App.jsx
@@ -8,6 +8,7 @@ import ExplorePage from './pages/ExplorePage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
+import UserProvider from './contexts/UserProvider';
 
 function App() {
   return (
@@ -15,15 +16,17 @@ function App() {
       <BrowserRouter>
         <FlashProvider>
           <ApiProvider>
-            <Header />
-            <Routes>
-              <Route path='/' element={<FeedPage />} />
-              <Route path='/explore' element={<ExplorePage />} />
-              <Route path='/user/:username' element={<UserPage />} />
-              <Route path='/login' element={<LoginPage />} />
-              <Route path='/register' element={<RegistrationPage />} />
-              <Route path='*' element={<Navigate to="/" />} />
-            </Routes>
+            <UserProvider>
+              <Header />
+              <Routes>
+                <Route path='/' element={<FeedPage />} />
+                <Route path='/explore' element={<ExplorePage />} />
+                <Route path='/user/:username' element={<UserPage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegistrationPage />} />
+                <Route path='*' element={<Navigate to="/" />} />
+              </Routes>
+            </UserProvider>
           </ApiProvider>
         </FlashProvider>  
       </BrowserRouter>
