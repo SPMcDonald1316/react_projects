5092f62c0c53b15b7d50894e75e67ec9aa598c27
sean <spmcdonald1316@gmail.com>
2024-07-31 13:52:36 -0500

    add authorization header to requests, create login method to get access token and logout method to remove access token

--- src/MicroblogApiClient.js
+++ src/MicroblogApiClient.js
@@ -2,7 +2,7 @@ const BASE_API_URL = 'http://localhost:4000';
 
 class MicroblogApiClient {
   constructor() {
-    this.base_url = BASE_API_URL + '/api';
+    this.base_url = `${BASE_API_URL}/api`;
   }
 
   async request(options) {
@@ -17,6 +17,7 @@ class MicroblogApiClient {
         method: options.method,
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('acessToken')}`,
           ...options.headers,
         },
         body: options.body ? JSON.stringify(options.body) : null,
@@ -56,6 +57,24 @@ class MicroblogApiClient {
   async delete(url, options) {
     return this.request({method: 'DELETE', url, ...options});
   } 
+
+  async login(username, password) {
+    const response = await this.post('/tokens', null, {
+      headers: {
+        Auhtorization: `Basic ${btoa(`${username}:${password}`)}`
+      }
+    });
+    if (!response.ok) {
+      return response.status === 401 ? 'fail' : 'error';
+    }
+    localStorage.setItem('accessToken', response.body.access_token);
+    return 'ok';
+  }
+
+  async logout() {
+    await this.delete('/tokens');
+    localStorage.removeItem('accessToken');
+  }
 }
 
 export default MicroblogApiClient
