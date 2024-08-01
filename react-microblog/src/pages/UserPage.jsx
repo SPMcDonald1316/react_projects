6c4c2751c1e8aaff8558eb7f9e4edf916861bb19
sean <spmcdonald1316@gmail.com>
2024-08-01 15:39:12 -0500

    add logic to display edit/follow/unfollow button based on page being viewed

--- react-microblog/src/pages/UserPage.jsx
+++ react-microblog/src/pages/UserPage.jsx
@@ -1,7 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useApi } from '../contexts/ApiProvider';
-import { Stack, Image, Spinner } from "react-bootstrap";
+import { useUser } from "../contexts/UserProvider";
+import { useFlash } from "../contexts/FlashProvider";
+import { Stack, Image, Spinner, Button } from "react-bootstrap";
 import Body from '../components/Body';
 import TimeAgo from '../components/TimeAgo';
 import Posts from '../components/Posts';
@@ -10,13 +12,43 @@ function UserPage() {
   const { username } = useParams();
   const [user, setUser] = useState();
   const api = useApi();
+  const [isFollower, setIsFollower] = useState();
+  const { user: loggedInUser } = useUser();
+  const flash = useFlash();
+  const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
       const response = await api.get(`/users/${username}`);
-      setUser(response.ok ? response.body : null);
+      if (response.ok) {
+        setUser(response.body);
+        if (response.body.username !== loggedInUser.username) {
+          const follower = await api.get(`/me/following/${response.body.id}`);
+          if (follower.status === 204) {
+            setIsFollower(true);
+          } else if (follower.status === 404) {
+            setIsFollower(false);
+          } else {
+            setIsFollower(null);
+          }
+        }
+      } else {
+        setUser(null);
+      }
     })();
-  }, [username, api]);
+  }, [username, api, loggedInUser]);
+
+  const edit = () => {
+    // TODO
+  };
+
+  const follow = async () => {
+    // TODO
+  };
+
+  const unfollow = async () => {
+    // TODO
+  };
 
   return (
     <Body sidebar>
@@ -38,6 +70,21 @@ function UserPage() {
                     <br />
                     Last seen: <TimeAgo isoDate={user.last_seen} />
                   </p>
+                  {isFollower === null &&
+                    <Button variant="primary" onClick={edit}>
+                      Edit
+                    </Button>
+                  }
+                  {isFollower === false &&
+                    <Button variant="primary" onClick={follow}>
+                      Follow
+                    </Button>
+                  }
+                  {isFollower === true &&
+                    <Button variant="primary" onClick={unfollow}>
+                      Unfollow
+                    </Button>
+                  }
                 </div>
               </Stack>
               <Posts content={user.id} />
