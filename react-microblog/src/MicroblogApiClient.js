90132659a081d54e52ab2a8b12378cf8779e7707
sean <spmcdonald1316@gmail.com>
2024-08-05 11:28:58 -0500

    add global error handling for unresponsive server errors

--- react-microblog/src/MicroblogApiClient.js
+++ react-microblog/src/MicroblogApiClient.js
@@ -1,7 +1,8 @@
 const BASE_API_URL = 'http://localhost:4000';
 
 class MicroblogApiClient {
-  constructor() {
+  constructor(onError) {
+    this.onError = onError
     this.base_url = `${BASE_API_URL}/api`;
   }
 
@@ -15,6 +16,9 @@ class MicroblogApiClient {
         localStorage.setItem('accessToken', refreshResponse.body.access_token);
         response = await this.requestInternal(options);
       }
+    }
+    if (response.status >= 500 && this.onError) {
+      this.onError(response);
     }
     return response;
   }
--- react-microblog/src/contexts/ApiProvider.jsx
+++ react-microblog/src/contexts/ApiProvider.jsx
@@ -1,10 +1,17 @@
 import { createContext, useContext } from "react";
 import MicroblogApiClient from '../MicroblogApiClient'
+import { useFlash } from './FlashProvider';
 
 const ApiContext = createContext();
 
 export default function ApiProvider({ children }) {
-  const api = new MicroblogApiClient();
+  const flash = useFlash();
+
+  const onError = () => {
+    flash('An unexpected error has occured. Please try again.', 'danger');
+  }
+
+  const api = new MicroblogApiClient(onError);
 
   return (
     <ApiContext.Provider value={api}>
