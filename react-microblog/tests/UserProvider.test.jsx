7965a26c9692c165026748aba2724cbff927d701
sean <spmcdonald1316@gmail.com>
2024-08-07 14:29:05 -0500

    login test

--- react-microblog/tests/UserProvider.test.jsx
+++ react-microblog/tests/UserProvider.test.jsx
@@ -15,3 +15,51 @@ afterEach(() => {
   global.fetch = realFetch;
   localStorage.clear();
 });
+
+test('logs user in', async () => {
+  const urls = [];
+
+  global.fetch
+    .mockImplementationOnce(url => {
+      urls.push(url);
+      return {
+        status: 200,
+        ok: true,
+        json: () => Promise.resolve({access_token: '123'}),
+      };
+    })
+    .mockImplementationOnce(url => {
+      urls.push(url);
+      return {
+        status: 200,
+        ok: true,
+        json: () => Promise.resolve({username: 'susan'}),
+      };
+    }
+  );
+
+  const Test = () => {
+    const {login, user} = useUser();
+    useEffect(() => {
+      (async () => await login('username', 'password'))();
+    }, []);
+    return user ? <p>{user.username}</p> : null;
+  };
+
+  render(
+    <FlashProvider>
+      <ApiProvider>
+        <UserProvider>
+          <Test />
+        </UserProvider>
+      </ApiProvider>
+    </FlashProvider>
+  );
+
+  const element = await screen.findByText('susan');
+  expect(element).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(2);
+  expect(urls).toHaveLength(2);
+  expect(urls[0]).toMatch(/^http.*\/api\/tokens$/);
+  expect(urls[1]).toMatch(/^http.*\/api\/me$/);
+});
