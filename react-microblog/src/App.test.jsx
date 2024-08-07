d68ed7e7b7298b3c8bac7935ac28896b643e16c9
sean <spmcdonald1316@gmail.com>
2024-08-07 11:05:25 -0500

    write first test to check for header text and class

---
+++ react-microblog/src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import App from './App'
+import { expect, test } from 'vitest';
+
+test('renders brand element', () => {
+  render(<App />);
+  const element = screen.getByText(/Microblog/);
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveClass('navbar-brand');
+});
+
