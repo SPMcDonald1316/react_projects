2f3b69c2ef3a3d0d4deb1943243dfd1091bce91a
sean <spmcdonald1316@gmail.com>
2024-08-12 15:08:20 -0500

    create redux authentication slice

---
+++ learn_react18_redux/src/_store/auth.slice.js
@@ -0,0 +1,80 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { history, fetchWrapper } from '../_helpers';
+
+// create slice
+const name = 'auth';
+const initialState = createInitialState();
+const reducers = createReducers();
+const extraActions = createExtraActions();
+const extraReducers = createExtraReducers();
+const slice = createSlice({ name, initialState, reducers, extraReducers });
+
+// exports
+
+export const authActions = { ...slice.actions, ...extraActions };
+export const authReducer = slice.reducer;
+
+// implementation
+
+function createInitialState() {
+  return {
+    // initialize state from local storage to enable user to stay logged in
+    user: JSON.parse(localStorage.getItem('user')),
+    error: null
+  };
+}
+
+function createReducers() {
+  return {
+    logout
+  };
+
+  function logout(state) {
+    state.user = null;
+    localStorage.removeItem('user');
+    history.navigate('/login');
+  }
+}
+
+function createExtraActions() {
+  const baseUrl = `http://localhost:4000/users`;
+  return {
+    login: login()
+  };
+
+  function login() {
+    return createAsyncThunk(
+      `${name}/login`,
+      async ({ username, password }) => await fetchWrapper.post(`${baseUrl}/authenticate`, { username, password })
+    );
+  }
+}
+
+function createExtraReducers() {
+  return {
+    ...login()
+  };
+
+  function login() {
+    var { pending, fullfilled, rejected } = extraActions.login;
+    return {
+      [pending]: (state) => {
+        state.error = null;
+      },
+      [fullfilled]: (state, action) => {
+        const user = action.payload;
+
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        localStorage.setItem('user', JSON.stringify(user));
+        state.user = user;
+
+        // get return url from location state or default to home page
+        const { from } = history.location.state || { from: { pathname: '/' } };
+        history.navigate(from)
+      },
+      [rejected]: (state, action) => {
+        state.error = action.error;
+      }
+    };
+  }
+}
