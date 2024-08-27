2cc81047a51ada0aad291b9d80cfedecf58a0133
sean <spmcdonald1316@gmail.com>
2024-08-27 11:48:07 -0500

    remove unwanted project

--- trello_clone_frontend/src/Reducers/Reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { routeReducer } from "react-router-redux";
-import session from './Session';
-
-export default combineReducers({
-  routing: routeReducer,
-  session: session,
-});
