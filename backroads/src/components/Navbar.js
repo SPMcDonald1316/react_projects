a48d61809359e020afb32e95c01af91815a93484
sean <spmcdonald1316@gmail.com>
2024-08-27 14:07:15 -0500

    complete navbar component

--- backroads/src/components/Navbar.js
+++ backroads/src/components/Navbar.js
@@ -1,4 +1,41 @@
+import logo from '../images/logo.svg';
+import { pageLinks, socialLinks } from '../data';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
+          </button>
+        </div>
+        <ul className='nav-links' id='nav-links'>
+          {pageLinks.map((link) => (
+            <li key={link.id}>
+              <a href={link.href} className='nav-link'>
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className='nav-icons'>
+          {socialLinks.map((link) => (
+            <li key={link.id}>
+              <a
+                href={link.href}
+                target='_blank'
+                className='nav-icon'
+                rel='noreferrer'
+              >
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default NavBar;
