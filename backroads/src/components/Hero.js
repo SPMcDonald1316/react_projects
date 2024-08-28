d7b6db2032d93d7ec890ab6c8527f07bc490121f
sean <spmcdonald1316@gmail.com>
2024-08-28 09:41:23 -0500

    complete hero component, fix background image path in css

--- backroads/src/components/Hero.js
+++ backroads/src/components/Hero.js
@@ -1,4 +1,18 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='hero' id='home'>
+      <div className='hero-banner'>
+        <h1>Backroads App</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          vehicula ac velit sit amet auctor. Sed luctus sem non justo vehicula,
+          semper sodales nisi elementum. Duis tincidunt congue nisi, vitae.
+        </p>
+        <a href='#tours' className='btn hero-btn'>
+          Explore Tours
+        </a>
+      </div>
+    </section>
+  );
 };
 export default Hero;
