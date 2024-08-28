311380133d3f0a0de19967fd7d895e7fed7e2b61
sean <spmcdonald1316@gmail.com>
2024-08-28 10:47:45 -0500

    add service descriptions to data, create service component, refactor services component to use service component

--- backroads/src/components/Services.js
+++ backroads/src/components/Services.js
@@ -1,4 +1,6 @@
 import Title from './Title';
+import Service from './Service';
+import { services } from '../data';
 
 const Sevices = () => {
   return (
@@ -6,44 +8,14 @@ const Sevices = () => {
       <Title title='our' spanContent='services' />
 
       <div className='section-center services-center'>
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fa-solid fa-wallet'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>saving money</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              convallis, urna ac pretium posuere, justo.
-            </p>
-          </div>
-        </article>
-
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fa-solid fa-tree'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>endless hiking</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              convallis, urna ac pretium posuere, justo.
-            </p>
-          </div>
-        </article>
-
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fa-solid fa-socks'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>amazing comfort</h4>
-            <p className='service-text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              convallis, urna ac pretium posuere, justo.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => (
+          <Service
+            key={service.id}
+            icon={service.icon}
+            title={service.title}
+            description={service.description}
+          />
+        ))}
       </div>
     </section>
   );
--- backroads/src/data.js
+++ backroads/src/data.js
@@ -10,3 +10,27 @@ export const socialLinks = [
   { id: 6, href: 'www.twitter.com', icon: 'fab fa-twitter' },
   { id: 7, href: 'www.squarespace.com', icon: 'fab fa-squarespace' },
 ];
+
+export const services = [
+  {
+    id: 8,
+    icon: 'fa-solid fa-wallet',
+    title: 'saving money',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, urna ac pretium posuere, justo.',
+  },
+  {
+    id: 9,
+    icon: 'fa-solid fa-tree',
+    title: 'endless hiking',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, urna ac pretium posuere, justo.',
+  },
+  {
+    id: 10,
+    icon: 'fa-solid fa-socks',
+    title: 'amazing comfort',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis, urna ac pretium posuere, justo.',
+  },
+];
