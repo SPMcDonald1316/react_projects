9b4d0e8391c8710f5138b9ee78195403c43c4b15
sean <spmcdonald1316@gmail.com>
2024-08-28 10:20:59 -0500

    build services component

--- backroads/src/components/Services.js
+++ backroads/src/components/Services.js
@@ -1,4 +1,51 @@
+import Title from './Title';
+
 const Sevices = () => {
-  return <div>Sevices</div>;
+  return (
+    <section className='section services' id='services'>
+      <Title title='our' spanContent='services' />
+
+      <div className='section-center services-center'>
+        <article className='service'>
+          <span className='service-icon'>
+            <i className='fa-solid fa-wallet'></i>
+          </span>
+          <div className='service-info'>
+            <h4 className='service-title'>saving money</h4>
+            <p className='service-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              convallis, urna ac pretium posuere, justo.
+            </p>
+          </div>
+        </article>
+
+        <article className='service'>
+          <span className='service-icon'>
+            <i className='fa-solid fa-tree'></i>
+          </span>
+          <div className='service-info'>
+            <h4 className='service-title'>endless hiking</h4>
+            <p className='service-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              convallis, urna ac pretium posuere, justo.
+            </p>
+          </div>
+        </article>
+
+        <article className='service'>
+          <span className='service-icon'>
+            <i className='fa-solid fa-socks'></i>
+          </span>
+          <div className='service-info'>
+            <h4 className='service-title'>amazing comfort</h4>
+            <p className='service-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              convallis, urna ac pretium posuere, justo.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 export default Sevices;
