73df2807b190ed2d2fe40001925d72ccf02c0c6a
sean <spmcdonald1316@gmail.com>
2024-09-04 10:14:16 -0500

    render list of birthdays, add button to clear list

--- birthday_buddy/src/App.jsx
+++ birthday_buddy/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import data from './data';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [birthdays, setBirthdays] = useState(data);
+
+  const clear = () => {
+    setBirthdays([]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='container'>
+      <h3>{birthdays.length} birthdays today</h3>
+      {birthdays.map((birthday) => {
+        return (
+          <div key={birthday.id} className='person'>
+            <img src={birthday.image}></img>
+            <h4>{birthday.name}</h4>
+            <p>{birthday.age} years</p>
+          </div>
+        );
+      })}
+      <button type='button' className='btn' onClick={clear}>
+        clear all
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
