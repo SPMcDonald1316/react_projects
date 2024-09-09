ddb40464565644fe823350cc414021ea3c082f2a
sean <spmcdonald1316@gmail.com>
2024-09-09 16:13:29 -0500

    refactor to use css

--- tours/src/components/Tours.jsx
+++ tours/src/components/Tours.jsx
@@ -2,11 +2,17 @@ import Tour from './Tour';
 
 const Tours = ({ tours }) => {
   return (
-    <div>
-      {tours.map((tour) => {
-        return <Tour key={tour.id} {...tour} />;
-      })}
-    </div>
+    <section>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='title-underline'></div>
+      </div>
+      <div className='tours'>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
   );
 };
 
