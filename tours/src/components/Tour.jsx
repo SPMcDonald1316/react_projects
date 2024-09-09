a1892dbda80d14fc17d8371cc66fd33970827036
sean <spmcdonald1316@gmail.com>
2024-09-09 16:26:16 -0500

    refactor to align with css

--- tours/src/components/Tour.jsx
+++ tours/src/components/Tour.jsx
@@ -6,10 +6,14 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
       <div className='tour-info'>
         <h5>{name}</h5>
         <p>{info}</p>
+        <button
+          type='button'
+          className='btn btn-block delete-btn'
+          onClick={() => removeTour(id)}
+        >
+          remove
+        </button>
       </div>
-      <button type='button' className='btn' onClick={() => removeTour(id)}>
-        remove
-      </button>
     </article>
   );
 };
