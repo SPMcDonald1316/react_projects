63e3cab552bd6030a8c49bfc396fb171fe228b8b
sean <spmcdonald1316@gmail.com>
2024-09-09 16:04:45 -0500

    build out Tours component

--- tours/src/components/Tours.jsx
+++ tours/src/components/Tours.jsx
@@ -1,4 +1,13 @@
-const Tours = () => {
-  return <div>Tours</div>;
+import Tour from './Tour';
+
+const Tours = ({ tours }) => {
+  return (
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} />;
+      })}
+    </div>
+  );
 };
+
 export default Tours;
