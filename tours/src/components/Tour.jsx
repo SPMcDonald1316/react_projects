8915ed235113d1b5ff84f1fa6310e133a553df94
sean <spmcdonald1316@gmail.com>
2024-09-09 16:05:24 -0500

    build out individual Tour component

--- tours/src/components/Tour.jsx
+++ tours/src/components/Tour.jsx
@@ -1,4 +1,11 @@
-const Tour = () => {
-  return <div>Tour</div>;
+const Tour = ({ image, info, name, price }) => {
+  return (
+    <div>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>{info}</p>
+      <p>{price}</p>
+    </div>
+  );
 };
 export default Tour;
