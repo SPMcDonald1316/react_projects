f6acf94706f966dc3b17063909eb2ab5dd45512c
sean <spmcdonald1316@gmail.com>
2024-09-09 15:59:59 -0500

    fetch data from api, setup conditional rendering while data is being fetched

--- tours/src/App.jsx
+++ tours/src/App.jsx
@@ -1,7 +1,34 @@
 const url = 'https://www.course-api.com/react-tours-project';
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
-};
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTours(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(tours);
+
+  return (
+    <>
+      {isLoading ? <Loading msg='Loading Data...' /> : <Tours tours={tours} />}
+    </>
+  );
+};
 export default App;
