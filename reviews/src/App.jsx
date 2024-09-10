5a6c535575243deef60cba63750ccf3d0f7ed3d5
sean <spmcdonald1316@gmail.com>
2024-09-10 14:58:44 -0500

    display single review

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 import people from './data';
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  return <h2>Reviews Starter</h2>;
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} className='person-img' alt={name} />
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+      </article>
+    </main>
+  );
 };
 
 export default App;
