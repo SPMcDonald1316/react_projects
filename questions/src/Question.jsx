5ed66ed5add1b35fee252ca64dc9a5ced5fb4f51
sean <spmcdonald1316@gmail.com>
2024-09-12 13:54:58 -0500

    render question, button, and text

--- questions/src/Question.jsx
+++ questions/src/Question.jsx
@@ -1,5 +1,15 @@
-const Question = () => {
-  return <h3>This is a question</h3>;
+const Question = ({ title, info }) => {
+  return (
+    <div className='question'>
+      <header>
+        <h5>{title}</h5>
+        <button type='button' className='question-btn'>
+          +
+        </button>
+      </header>
+      <p>{info}</p>
+    </div>
+  );
 };
 
 export default Question;
