f54647d9896bb4d1747d6de8f9a252aac89c2c51
sean <spmcdonald1316@gmail.com>
2024-09-23 17:01:56 -0500

    add functionality to move forward/backward between slides

--- slider/src/Carousel.jsx
+++ slider/src/Carousel.jsx
@@ -5,9 +5,20 @@ import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const Carousel = () => {
   const [people, setPeople] = useState(longList);
+  const [current, setCurrent] = useState(1);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrent((old) => {
+      const result = (old - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrent((old) => {
+      const result = (old + 1) % people.length;
+      return result;
+    });
+  };
 
   return (
     <section className='slider-container'>
@@ -16,7 +27,7 @@ const Carousel = () => {
         return (
           <article
             className='slide'
-            style={{ transform: `translateX(${100 * index}%)` }}
+            style={{ transform: `translateX(${100 * (index - current)}%)` }}
             key={id}
           >
             <img src={image} alt={name} className='person-img' />
