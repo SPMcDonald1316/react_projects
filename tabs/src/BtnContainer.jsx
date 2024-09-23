680e6d52e6bd880167f8ebd13f03b9dd9b9e7d34
sean <spmcdonald1316@gmail.com>
2024-09-23 15:35:51 -0500

    display button to display each job

--- tabs/src/BtnContainer.jsx
+++ tabs/src/BtnContainer.jsx
@@ -1,3 +1,15 @@
-const BtnContainer = () => {};
+const BtnContainer = ({ jobs }) => {
+  return (
+    <div className='btn-container'>
+      {jobs.map((job) => {
+        return (
+          <button key={job.id} type='button' className='job-btn'>
+            {job.company}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 export default BtnContainer;
