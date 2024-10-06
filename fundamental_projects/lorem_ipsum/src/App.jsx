f124a03433c213473e4103b47530dbc7b1bb689b
sean <spmcdonald1316@gmail.com>
2024-10-06 12:59:04 -0500

    solution with css

--- fundamental_projects/lorem_ipsum/src/App.jsx
+++ fundamental_projects/lorem_ipsum/src/App.jsx
@@ -3,8 +3,10 @@ import text from './data';
 
 const App = () => {
   const [count, setCount] = useState(1);
-  const [paragraphs, setParagraphs] = useState(text[0]);
+  const [text, setText] = useState([]);
 
+  {
+    /* MY SOLUTION
   const handleChange = (e) => {
     setCount(e.target.value);
   };
@@ -37,6 +39,30 @@ const App = () => {
       </form>
       <div>{paragraphs}</div>
     </div>
+  );
+  */
+  }
+
+  return (
+    <section className='section-center'>
+      <h4>tired of boring lorem ipsum</h4>
+      <form className='lorem-form'>
+        <label htmlFor='amount'>paragraphs</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min='1'
+          max='8'
+          step='1'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+    </section>
   );
 };
 export default App;
