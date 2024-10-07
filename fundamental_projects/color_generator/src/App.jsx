ffa577c60dd80547366443795c79da4682230a17
sean <spmcdonald1316@gmail.com>
2024-10-06 23:52:26 -0500

    setup react-toastify for success/error messages

--- fundamental_projects/color_generator/src/App.jsx
+++ fundamental_projects/color_generator/src/App.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react';
 import ColorList from './components/ColorList';
 import Form from './components/Form';
 import Values from 'values.js';
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#35274e').all(10));
 
   const changeColorList = (color) => {
-    setColors(new Values(color).all(10));
+    try {
+      setColors(new Values(color).all(10));
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
     <main>
       <Form changeColorList={changeColorList} />
       <ColorList colors={colors} />
+      <ToastContainer position='top-center' />
     </main>
   );
 };
