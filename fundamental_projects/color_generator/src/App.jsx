5c8adb5319e694fd306a58c8736701f98670ee92
sean <spmcdonald1316@gmail.com>
2024-10-06 23:27:40 -0500

    add values.js library, setup state variable in app

--- fundamental_projects/color_generator/src/App.jsx
+++ fundamental_projects/color_generator/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import ColorList from './components/ColorList';
 import Form from './components/Form';
+import Values from 'values.js';
 
 const App = () => {
+  const [colors, setColors] = useState(new Values('#35274e').all(10));
+
+  const colorList = (color) => {
+    setColors(new Values(color).all(10));
+  };
+
   return (
     <main>
-      <Form />
+      <Form colorList={colorList} />
       <ColorList />
     </main>
   );
