caaa6132634b710b06743936ee285fe83d8a1911
sean <spmcdonald1316@gmail.com>
2024-10-14 14:34:49 -0500

    begin navbar component

--- fundamental_projects/navbar/src/App.jsx
+++ fundamental_projects/navbar/src/App.jsx
@@ -1,4 +1,11 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <h2>Navbar Starter</h2>;
+  return (
+    <main>
+      <h2>Navbar Starter</h2>
+      <Navbar />
+    </main>
+  );
 };
 export default App;
