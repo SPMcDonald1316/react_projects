82be596a694293232f866f786ded0160bf48e071
sean <spmcdonald1316@gmail.com>
2024-10-14 15:46:05 -0500

    use useRef hook to render links container dynamically

--- fundamental_projects/navbar/src/components/Navbar.jsx
+++ fundamental_projects/navbar/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { links, social } from '../data';
 import logo from '../logo.svg';
 
+// USING USE REF TO DYNAMICALLY SET CONTAINER
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  };
 
   return (
     <nav>
@@ -19,11 +28,11 @@ const Navbar = () => {
           </button>
         </div>
         <div
-          className={
-            showLinks ? 'links-container show-container' : 'links-container'
-          }
+          className='links-container'
+          ref={linksContainerRef}
+          style={linkStyles}
         >
-          <ul className='links'>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               return (
                 <li key={link.id}>
@@ -39,7 +48,10 @@ const Navbar = () => {
 };
 export default Navbar;
 
-/* SHOW LINKS DYNAMICALLY USING STATE VALUE EXPLICITLY
+/* 
+SHOW LINKS DYNAMICALLY BY USING STATE VALUE
+TO RENDER CONDITIONALLY
+
 {showLinks && (
           <div className='links-container'>
             <ul className='links'>
@@ -52,5 +64,26 @@ export default Navbar;
               })}
             </ul>
           </div>
-        )}
+)}
+*/
+
+/* 
+SHOW LINKS USING CSS CLASSES ADDED CONDITIONALLY BASED
+ON STATE VALUE
+
+THIS APPROACH USES A FIXED HEIGHT VALUE IN CSS ONLY WORKS
+IF HEIGHT OF CONTAINER ISNT GOING TO CHANGE (IE NEW LINKS ADDED WON"T SHOW)
+
+<div className={ showLinks ? 'links-container show-container' : 'links-container' }>
+  <ul className='links'>
+    {links.map((link) => {
+      return (
+        <li key={link.id}>
+          <a href={link.url}>{link.text}</a>
+        </li>
+      );
+    })}
+  </ul>
+</div>
+
 */
