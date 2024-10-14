05cf22f432b1b72eed2836f0dfa405bb78cbdfe9
sean <spmcdonald1316@gmail.com>
2024-10-14 15:04:36 -0500

    build out navbar component to use logo, icon, and add css classes

--- fundamental_projects/navbar/src/App.jsx
+++ fundamental_projects/navbar/src/App.jsx
@@ -1,11 +1,9 @@
 import Navbar from './components/Navbar';
-import { links } from './data';
 
 const App = () => {
   return (
     <main>
-      <h2>Navbar Starter</h2>
-      <Navbar links={links} />
+      <Navbar />
     </main>
   );
 };
--- fundamental_projects/navbar/src/components/Navbar.jsx
+++ fundamental_projects/navbar/src/components/Navbar.jsx
@@ -1,22 +1,38 @@
 import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from '../data';
+import logo from '../logo.svg';
 
-const Navbar = ({ links }) => {
+const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
 
   return (
-    <section>
-      {showLinks &&
-        links.map((link) => {
-          return (
-            <div key={link.id}>
-              <a href={link.url}>{link.text}</a>
-            </div>
-          );
-        })}
-      <button type='button' onClick={() => setShowLinks(!showLinks)}>
-        show links
-      </button>
-    </section>
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='coding addict logo' className='logo' />
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className='links-container'>
+            <ul className='links'>
+              {links.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <a href={link.url}>{link.text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
   );
 };
 export default Navbar;
