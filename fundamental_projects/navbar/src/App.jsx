9b6d936df2e8ed9da1446fbcecea7509aad0b779
sean <spmcdonald1316@gmail.com>
2024-10-14 14:41:11 -0500

    import links, render conditionally based on state value, create button to change state value

--- fundamental_projects/navbar/src/App.jsx
+++ fundamental_projects/navbar/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar';
+import { links } from './data';
 
 const App = () => {
   return (
     <main>
       <h2>Navbar Starter</h2>
-      <Navbar />
+      <Navbar links={links} />
     </main>
   );
 };
--- fundamental_projects/navbar/src/components/Navbar.jsx
+++ fundamental_projects/navbar/src/components/Navbar.jsx
@@ -1,4 +1,22 @@
-const Navbar = () => {
-  return <h2>Navbar</h2>;
+import { useState } from 'react';
+
+const Navbar = ({ links }) => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <section>
+      {showLinks &&
+        links.map((link) => {
+          return (
+            <div key={link.id}>
+              <a href={link.url}>{link.text}</a>
+            </div>
+          );
+        })}
+      <button type='button' onClick={() => setShowLinks(!showLinks)}>
+        show links
+      </button>
+    </section>
+  );
 };
 export default Navbar;
