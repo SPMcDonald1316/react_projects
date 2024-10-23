3ec8ef731fe4e59ac8bce836b352efa46455e4f1
sean <spmcdonald1316@gmail.com>
2024-10-23 16:05:16 -0500

    setup and style navbar component

--- fundamental_projects/strapi_submenus/src/components/Navbar.jsx
+++ fundamental_projects/strapi_submenus/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
+import { FaBars } from 'react-icons/fa';
+import { useAppContext } from '../context';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { openSidebar } = useAppContext();
+  return (
+    <nav>
+      <div className='nav-center'>
+        <h3 className='logo'>strapi</h3>
+        <button className='toggle-btn' onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* nav links */}
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
