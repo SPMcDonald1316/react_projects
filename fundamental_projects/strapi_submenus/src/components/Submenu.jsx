adfa3d2eca24120bbf1ad386669e6e536d2fdde5
sean <spmcdonald1316@gmail.com>
2024-11-02 12:25:56 -0500

    being submenu setup to display content based on hover/pageid

--- fundamental_projects/strapi_submenus/src/components/Submenu.jsx
+++ fundamental_projects/strapi_submenus/src/components/Submenu.jsx
@@ -1,4 +1,25 @@
+import { useAppContext } from '../context';
+import sublinks from '../data';
+
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId } = useAppContext();
+  const currentPage = sublinks.find((page) => page.pageId === pageId);
+
+  return (
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div className='submenu-links'>
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
