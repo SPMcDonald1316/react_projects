eefed72befd3966f5b0eb5d7136bb1b61f4f529e
sean <spmcdonald1316@gmail.com>
2024-11-09 13:06:43 -0600

    render components to main app

--- fundamental_projects/cart/src/App.jsx
+++ fundamental_projects/cart/src/App.jsx
@@ -1,8 +1,12 @@
+import CartContainer from './CartContainer';
+import Navbar from './Navbar';
+
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
