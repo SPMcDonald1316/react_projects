7623097a539ca057d662b4c97d83838ffbc8df1c
sean <spmcdonald1316@gmail.com>
2025-02-03 14:29:51 -0600

    use local storage to save user theme preferences, project complete

--- unsplash-images/src/context.jsx
+++ unsplash-images/src/context.jsx
@@ -2,17 +2,33 @@ import { createContext, useContext, useState, useEffect } from 'react';
 
 const GlobalContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme');
+
+  if (storedDarkMode === null) return prefersDarkMode;
+
+  return storedDarkMode === 'true';
+};
+
 export const useGlobalContext = () => useContext(GlobalContext);
 
 export const AppContext = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('cows');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-    document.body.classList.toggle('dark-theme', newDarkTheme);
+    console.log('setting local storage...');
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <GlobalContext.Provider
