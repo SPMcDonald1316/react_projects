402accdd6ba304673152377b12a69344ce7ec34a
sean <spmcdonald1316@gmail.com>
2025-02-03 13:07:38 -0600

    setup env variable for api key

--- unsplash-images/src/components/Gallery.jsx
+++ unsplash-images/src/components/Gallery.jsx
@@ -1,3 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+
+const url = `https://api.unsplash.com/search/photosclient_id=${
+  import.meta.env.VITE_API_KEY
+}`;
+
 const Gallery = () => {
   return <h2>Gallery</h2>;
 };
