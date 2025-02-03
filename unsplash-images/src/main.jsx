c5c1f93b24026b818491d1feab947cc8fe2c9562
sean <spmcdonald1316@gmail.com>
2025-02-03 13:47:27 -0600

    add react query dev tools

--- unsplash-images/src/main.jsx
+++ unsplash-images/src/main.jsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.jsx';
 import { AppContext } from './context.jsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
   <AppContext>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </AppContext>
 );
