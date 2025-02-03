9a9c5278d62e347a7b2190cd9fc783af13a48419
sean <spmcdonald1316@gmail.com>
2025-02-03 13:45:38 -0600

    add react query and use to make call to unsplash api

--- unsplash-images/src/components/Gallery.jsx
+++ unsplash-images/src/components/Gallery.jsx
@@ -1,10 +1,58 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
-const url = `https://api.unsplash.com/search/photosclient_id=${
+const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_API_KEY
-}`;
+}&per_page=12&query=turtle`;
 
 const Gallery = () => {
-  return <h2>Gallery</h2>;
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className='image-container'>
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No results found.</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className='image-container'>
+      {results.map((item) => {
+        return (
+          <img
+            key={item.id}
+            src={item?.urls?.regular}
+            alt={item.alt_description}
+            className='img'
+          />
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
--- unsplash-images/src/main.jsx
+++ unsplash-images/src/main.jsx
@@ -1,10 +1,15 @@
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.jsx';
 import { AppContext } from './context.jsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <AppContext>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </AppContext>
 );
