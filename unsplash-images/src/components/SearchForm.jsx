910d90b74d7d39b894c69d46a186f79f03721c8c
sean <spmcdonald1316@gmail.com>
2025-02-03 12:44:40 -0600

    build out search form

--- unsplash-images/src/components/SearchForm.jsx
+++ unsplash-images/src/components/SearchForm.jsx
@@ -1,4 +1,26 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className='title'>unsplash images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='search'
+          placeholder='cat'
+          className='form-input search-input'
+        />
+        <button type='submit' className='btn'>
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
