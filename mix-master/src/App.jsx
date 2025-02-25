e93531eb852f524b60c594fe9d915011521baa41
sean <spmcdonald1316@gmail.com>
2025-02-25 14:16:37 -0600

    setup basic router

--- mix-master/src/App.jsx
+++ mix-master/src/App.jsx
@@ -1,4 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h2>Home Page</h2>} />
+        <Route path='/about' element={<h2>About Page</h2>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
