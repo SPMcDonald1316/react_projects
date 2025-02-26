1d3e3e28a7781d5cfb81e8447118915c3bed1d68
sean <spmcdonald1316@gmail.com>
2025-02-26 15:36:46 -0600

    use loader to pre load landing route with data

--- mix-master/src/pages/Landing.jsx
+++ mix-master/src/pages/Landing.jsx
@@ -1,4 +1,18 @@
+import axios from 'axios';
+import { useLoaderData } from 'react-router';
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+export const loader = async () => {
+  const searchTerm = 'a';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm };
+};
+
 const Landing = () => {
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <h1>Landing</h1>;
 };
 export default Landing;
