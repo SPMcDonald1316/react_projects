0f5ade0b694a48eee9afb2f837e272f67fc5f2f0
sean <spmcdonald1316@gmail.com>
2025-03-03 15:46:07 -0600

    add react toastify library

--- mix-master/src/main.jsx
+++ mix-master/src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <ToastContainer position='top-center' autoClose={2000} />
+    <App />
+  </>
+);
