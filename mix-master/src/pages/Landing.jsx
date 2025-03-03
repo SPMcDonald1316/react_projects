c45e1419f0499da1c5e3d9144d2e587dd74a0155
sean <spmcdonald1316@gmail.com>
2025-03-03 16:40:26 -0600

    set up search functionality

--- mix-master/src/pages/Landing.jsx
+++ mix-master/src/pages/Landing.jsx
@@ -6,8 +6,9 @@ import SearchForm from '../components/SearchForm';
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-  const searchTerm = 'a';
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get('search') || 'a';
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: response.data.drinks, searchTerm };
 };
@@ -17,7 +18,7 @@ const Landing = () => {
 
   return (
     <>
-      <SearchForm />
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </>
   );
