ed93444a1a0f56f17530548f3b2666c660978f4b
sean <spmcdonald1316@gmail.com>
2025-03-03 14:04:52 -0600

    build out cocktail list component, add to landing page

--- mix-master/src/components/CocktailList.jsx
+++ mix-master/src/components/CocktailList.jsx
@@ -1,4 +1,28 @@
-const CocktailList = () => {
-  return <h2>CocktailList</h2>;
+import Wrapper from '../assets/wrappers/CocktailList';
+import CocktailCard from './CocktailCard';
+
+const CocktailList = ({ drinks }) => {
+  if (!drinks) {
+    return (
+      <h4 style={{ textAlign: 'center' }}>No matching cocktails found...</h4>
+    );
+  }
+  const formattedDrinks = drinks.map((item) => {
+    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+    return {
+      id: idDrink,
+      name: strDrink,
+      image: strDrinkThumb,
+      info: strAlcoholic,
+      glass: strGlass,
+    };
+  });
+  return (
+    <Wrapper>
+      {formattedDrinks.map((item) => {
+        return <CocktailCard key={item.id} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
 export default CocktailList;
