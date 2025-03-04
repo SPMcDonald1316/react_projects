c37010c8e10694c3a058e4a1c66d8c879ef7d382
sean <spmcdonald1316@gmail.com>
2025-03-04 11:32:39 -0600

    use queryClient in landing page loader

--- mix-master/src/pages/Landing.jsx
+++ mix-master/src/pages/Landing.jsx
@@ -17,11 +17,14 @@ const searchCocktailsQuery = (searchTerm) => {
   };
 };
 
-export const loader = async ({ request }) => {
-  const url = new URL(request.url);
-  const searchTerm = url.searchParams.get('search') || 'a';
-  return { searchTerm };
-};
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get('search') || 'all';
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+    return { searchTerm };
+  };
 
 const Landing = () => {
   const { searchTerm } = useLoaderData();
