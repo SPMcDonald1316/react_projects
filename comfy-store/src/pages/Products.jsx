abc28365131b351ea8ac661721619f96fa5ee191
sean <spmcdonald1316@gmail.com>
2025-03-15 13:50:22 -0500

    products page setup, create needed components, create loader, return needed data

--- comfy-store/src/pages/Products.jsx
+++ comfy-store/src/pages/Products.jsx
@@ -1,4 +1,20 @@
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+
+export const loader = async ({ request }) => {
+  const response = await customFetch('/products');
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta };
+};
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
 };
 export default Products;
