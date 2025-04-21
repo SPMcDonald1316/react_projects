29c96e98c8992f7c5b1cbb088bc6f6d551ccc49c
sean <spmcdonald1316@gmail.com>
2025-04-21 12:53:04 -0500

    connect filter inputs using params

--- comfy-store/src/pages/Products.jsx
+++ comfy-store/src/pages/Products.jsx
@@ -2,7 +2,10 @@ import { Filters, PaginationContainer, ProductsContainer } from '../components';
 import { customFetch } from '../utils';
 
 export const loader = async ({ request }) => {
-  const response = await customFetch('/products');
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const response = await customFetch('/products', { params });
   const products = response.data.data;
   const meta = response.data.meta;
   return { products, meta };
