c80a1732e2beb437225ececc93b97c89c4e5eeda
sean <spmcdonald1316@gmail.com>
2025-04-21 16:12:50 -0500

    begin setup for user slice, add to redux store

---
+++ comfy-store/src/features/user/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+
+const themes = {
+  winter: 'winter',
+  night: 'night',
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem('theme') || theme.winter;
+  document.documentElement.setAttribute('data-theme', theme);
+  return theme;
+};
+
+const initialState = {
+  user: { username: 'coding addict' },
+  theme: getThemeFromLocalStorage(),
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      console.log('login');
+    },
+    logoutUser: (state) => {
+      console.log('logout');
+    },
+    toggleTheme: (state) => {
+      const { winter, night } = themes;
+      state.theme = state.theme === night ? winter : night;
+      document.documentElement.setAttribute('data-theme', state.theme);
+      localStorage.setItem('theme', state.theme);
+    },
+  },
+});
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+export default userSlice.reducer;
