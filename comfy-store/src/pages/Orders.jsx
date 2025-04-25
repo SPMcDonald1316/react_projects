deece44bf706fa56e40d9c0fbb376d36a9529cef
sean <spmcdonald1316@gmail.com>
2025-04-25 15:56:05 -0500

    add a more complex pagination to orders page

--- comfy-store/src/pages/Orders.jsx
+++ comfy-store/src/pages/Orders.jsx
@@ -1,7 +1,11 @@
 import { redirect, useLoaderData } from 'react-router';
 import { toast } from 'react-toastify';
 import { customFetch } from '../utils';
-import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+} from '../components';
 
 export const loader =
   (store) =>
@@ -53,7 +57,7 @@ const Orders = () => {
     <>
       <SectionTitle text='Your Orders' />
       <OrdersList />
-      <PaginationContainer />
+      <ComplexPaginationContainer />
     </>
   );
 };
