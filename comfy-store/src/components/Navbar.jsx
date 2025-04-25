c2c1736064e19ef34c6fcce43afef7b06afe3e61
sean <spmcdonald1316@gmail.com>
2025-04-25 09:36:11 -0500

    refactor navbar to remove theme logic and connect it directly through redux

--- comfy-store/src/components/Navbar.jsx
+++ comfy-store/src/components/Navbar.jsx
@@ -2,31 +2,17 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router';
 import NavLinks from './NavLinks';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-
-const themes = {
-  winter: 'winter',
-  night: 'night',
-};
-
-const getTheme = () => {
-  return localStorage.getItem('theme') || themes.winter;
-};
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from '../features/user/userSlice';
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(getTheme());
+  const theme = useSelector((state) => state.userState.theme);
+  const isDarkTheme = theme === 'night';
   const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+  const dispatch = useDispatch();
   const handleTheme = () => {
-    const { winter, night } = themes;
-    const newTheme = theme === winter ? night : winter;
-    setTheme(newTheme);
+    dispatch(toggleTheme());
   };
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
 
   return (
     <nav className='bg-base-200'>
@@ -60,12 +46,16 @@ const Navbar = () => {
         <div className='navbar-end'>
           {/* THEME SETUP */}
           <label className='swap swap-rotate'>
-            <input type='checkbox' onChange={handleTheme} />
+            <input
+              type='checkbox'
+              onChange={handleTheme}
+              defaultChecked={isDarkTheme}
+            />
             <BsSunFill className='swap-on h-4 w-4' />
             <BsMoonFill className='swap-off h-4 w-4' />
           </label>
           {/* CART LINK */}
-          <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml=4'>
+          <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
               <BsCart3 className='h-6 w-6' />
               <span className='badge badge-sm badge-primary indicator-item'>
