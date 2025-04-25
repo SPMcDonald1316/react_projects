f8d5da2c07d9e351e0424937e460b738af04a915
sean <spmcdonald1316@gmail.com>
2025-04-25 11:30:12 -0500

    check user is logged in before accessing checkout page

--- comfy-store/src/pages/Checkout.jsx
+++ comfy-store/src/pages/Checkout.jsx
@@ -1,5 +1,18 @@
 import { useSelector } from 'react-redux';
-import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+import { SectionTitle, CartTotals } from '../components';
+import { toast } from 'react-toastify';
+import { redirect } from 'react-router';
+
+export const loader = (store) => async () => {
+  const user = store.getState().userState.user;
+
+  if (!user) {
+    toast.warn('You must be logged in to checkout');
+    return redirect('/login');
+  }
+
+  return null;
+};
 
 const Checkout = () => {
   const cartTotal = useSelector((state) => state.cartState.cartTotal);
@@ -12,7 +25,7 @@ const Checkout = () => {
     <>
       <SectionTitle text='Place your order' />
       <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
-        <CheckoutForm />
+        {/* <CheckoutForm /> */}
         <CartTotals />
       </div>
     </>
